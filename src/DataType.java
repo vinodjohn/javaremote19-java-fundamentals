427c745619e8fc6d4062fbc0c8bf87aeb7fa4410
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-09-14 20:05:22 +0300

    Added datatypes

---
+++ src/DataType.java
@@ -0,0 +1,33 @@
+/**
+ * Examples of Primitive datatype
+ *
+ * @author Vinod John
+ */
+public class DataType {
+    // Numbers
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 545454681351L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45454534836452374853746846848334438438438438438486748364389484384836438543854384f;
+    double doubleNumber = 3.456d;
+
+    // Character
+    char alpha = 'F';
+
+    //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    /* Multi-line
+    comment
+     */
+
+    int i; // 0
+
+    float x; // 0.00f
+
+    char animal; // ''
+
+    boolean isClassTomorrow;  // false
+}
