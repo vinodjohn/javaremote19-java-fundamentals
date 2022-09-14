c7d5f35be31feeb82ace8c62417a4fb204cb6a8e
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-09-14 20:53:46 +0300

    Added datatype examples

--- src/DataType.java
+++ src/DataType.java
@@ -4,6 +4,7 @@
  * @author Vinod John
  */
 public class DataType {
+    //Primitive Data types
     // Numbers
     int a = 1;
     byte byteNumber = 120;
@@ -30,4 +31,59 @@ public class DataType {
     char animal; // ''
 
     boolean isClassTomorrow;  // false
+    
+    
+    //Non-primitive data types
+    String word = "I'm a developer";
+    
+    Character random = 'u';
+    
+    Integer number = 5;
+    
+    Float numberSecond = 2.45F;
+    
+    Integer randomNumber;  // null
+
+    // Multi-declaration
+    int numberOne, numberTwo, numberThree;
+
+    String fruit = "apple", vegetable = "pumpkin", cities;
+
+
+    // Scope of class variables
+    private static String CAR = "Toyota";
+
+    public static String FOOD = "Cake";
+
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+        
+        
+        System.out.println(k);
+        System.out.println(y);
+
+
+        String _color = "black";
+
+        int $hexaNumber = 0x2A;
+
+        float exam12_mark = 100.10f;
+
+        String className = "sda";
+
+        int a = 100;
+        int b = 0;
+
+        int c = a + b;
+
+        System.out.println(CAR);
+        System.out.println(FOOD);
+    }
+
+
+
+
+    
 }
