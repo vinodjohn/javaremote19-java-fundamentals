34002a89ddc1e9fd75b06528a9b6471b11fddaf2
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-10-05 18:57:26 +0300

    Homework added

--- src/homeworks/IntegerDisplay.java
+++ src/homeworks/IntegerDisplay.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 /**
  * To read a number from user and print the same number like this:
  *
@@ -9,4 +11,27 @@ package homeworks;
  * @author VinodJohn
  */
 public class IntegerDisplay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        for(int i = 1; i <= number; i++) { // To print number of times/lines
+            for (int j=1; j <= i; j++) { // To print the number of digits
+                System.out.print(number);
+            }
+
+            System.out.println();
+        }
+
+        for(int i = number; i >= 1; i--) { // To print number of times/lines
+            for (int j=i; j >= 1; j--) { // To print the number of digits
+                System.out.print(number);
+            }
+
+            System.out.println();
+        }
+
+    }
 }
